e385e1737767f8edba564c33d2104c051687217f
Ivan <ivangybskiy03@gmail.com>
2023-01-31 21:19:40 +0300

    Scitala_Initialization
    +Scitala_Button&inputs

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import Scitala from './components/Scitala/Scitala'
+import './App.scss'
+function App() {
+	return (
+		<span className='App'>
+			<Scitala />
+		</span>
+	)
+}
+
+export default App
---
+++ src/components/Scitala/Scitala.tsx
@@ -0,0 +1,13 @@
+import './Scitala.scss'
+function Scitala() {
+	return (
+		<div className='RouterBreadcrumbs'>
+			<h2>Введите размер Скиталы:</h2>
+			<input type='text' className='scitalaInput_first' value={6} />
+			<input type='text' className='scitalaInput_second ' value={4} />
+			<button className='scitalaButton'>Задать</button>
+		</div>
+	)
+}
+
+export default Scitala
