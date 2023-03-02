eee11b232e992041990851fcaaf52ea7d2ff2c78
Ivan <ivangybskiy03@gmail.com>
2023-03-02 20:08:36 +0300

    Update Scitala.tsx

--- src/components/Scitala/Scitala.tsx
+++ src/components/Scitala/Scitala.tsx
@@ -6,6 +6,7 @@ function Scitala() {
 			<input type='text' className='scitalaInput_first' value={6} />
 			<input type='text' className='scitalaInput_second ' value={4} />
 			<button className='scitalaButton'>Задать</button>
+			<button className='scitalaButton'>Поменять метод шифровки</button>
 		</div>
 	)
 }
