f3063a1962f5d45a6ea7fb5542859d0f76c273fc
Ivan <ivangybskiy03@gmail.com>
2023-03-03 23:17:59 +0300

    Update Scitala.tsx

--- src/components/Scitala/Scitala.tsx
+++ src/components/Scitala/Scitala.tsx
@@ -1,12 +1,18 @@
 import './Scitala.scss'
 function Scitala() {
+	function handleSubmit(e: any) {
+		e.preventDefault()
+		console.log('You clicked submit.')
+	}
 	return (
 		<div className='RouterBreadcrumbs'>
 			<h2>Введите размер Скиталы:</h2>
 			<input type='text' className='scitalaInput_first' value={6} />
 			<input type='text' className='scitalaInput_second ' value={4} />
 			<button className='scitalaButton'>Задать</button>
-			<button className='scitalaButton'>Поменять метод шифровки</button>
+			<form onSubmit={handleSubmit}>
+				<button type='submit'>Submit</button>
+			</form>
 		</div>
 	)
 }
